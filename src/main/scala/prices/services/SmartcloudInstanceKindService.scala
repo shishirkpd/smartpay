b19b86b8ae7b42c8247de6181d705e8f426943fd
Shishir Kumar <shishir@Shishirs-MacBook-Air.local>
2022-01-08 20:33:34 +0800

    fixed the spacing

--- src/main/scala/prices/services/SmartcloudInstanceKindService.scala
+++ src/main/scala/prices/services/SmartcloudInstanceKindService.scala
@@ -8,16 +8,11 @@ import prices.data._
 
 object SmartcloudInstanceKindService {
 
-  final case class Config(
-                           baseUri: String,
-                           token: String
-                         )
+  final case class Config(baseUri: String, token: String)
 
   def make[F[_]: Concurrent](config: Config): InstanceKindService[F] = new SmartcloudInstanceKindService(config)
 
-  private final class SmartcloudInstanceKindService[F[_]: Concurrent](
-                                                                       config: Config
-                                                                     ) extends InstanceKindService[F] {
+  private final class SmartcloudInstanceKindService[F[_]: Concurrent](config: Config) extends InstanceKindService[F] {
 
     implicit val instanceKindsEntityDecoder: EntityDecoder[F, List[String]] = jsonOf[F, List[String]]
     implicit val instanceKindsWithPriceEntityDecoder: EntityDecoder[F, String] = jsonOf[F, String]
